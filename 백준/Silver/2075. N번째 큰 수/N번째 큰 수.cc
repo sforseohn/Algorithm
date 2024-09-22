5797f2a94d50476e01e6e7779130b9489cdcd297
sforseohn <100199530+sforseohn@users.noreply.github.com>
2024-09-22 21:31:00 +0900

    [Silver II] Title: N번째 큰 수, Time: 252 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,49 +1,35 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
-// 모든 수는 자신의 한 칸 위에 있는 수보다 크다. 
-// 표에 채워진 수는 모두 다르다.
+int main() {
+    int N;
+    cin >> N;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-// min_heap 우선순위큐 n_numbers 에는 딱 n개만 저장 -> 최종적으로 top이 n번째 큰 수가 되도록)
-// 새로 들어온 input과 top = q.top()을 비교할 것임
-// input > top 이면 input을 q에 추가하고, top을 q에서 제거함
-void checkNum(priority_queue<int, vector<int>, greater<int>> &n_numbers, int input){
-    if (input > n_numbers.top()){
-        n_numbers.push(input);
-        n_numbers.pop();    // 가장 작은 값을 제거하여 크기를 유지
-    }
-}
+    priority_queue<int, vector<int>, greater<int>> minHeap;
 
-int main(){
-    // 입출력 속도 향상
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int num;
+            cin >> num;
 
-    int n; // n: n*n개의 수가 주어진다 (1이상 1500이하)
-    int input; // input: 입력받는 정수 (n^2개 입력받음)
-    int answ; // answ: 출력할 n번째 큰 수
-    priority_queue<int, vector<int>, greater<int>> n_numbers; // n_numbers: answ 구하기 위한 min_heap 우선순위큐 
-
-    // 입력
-    cin >> n;
-    // n_numbers에 n개의 값 저장
-    for (int i = 0; i < n; i++){
-        cin >> input;
-        n_numbers.push(input);
-    }
-    for (int i = 0; i < n-1; i++){
-        for (int j = 0; j < n; j++){
-            cin >> input;
-            checkNum(n_numbers, input);
+            // 힙에 N개가 되기 전까지는 삽입
+            if (minHeap.size() < N) {
+                minHeap.push(num);
+            } else {
+                // N개 이상; 새로운 수가 힙의 최소값보다 크면 교체
+                if (num > minHeap.top()) {
+                    minHeap.pop();
+                    minHeap.push(num);
+                }
+            }
         }
     }
 
-    // 연산
-    answ = n_numbers.top();
-
-    // 출력
-    cout << answ;
-
+    // 이미 더 작은 N-1개를 pop()했으니 top()이 N번째 수
+    cout << minHeap.top() << endl;
     return 0;
 }
