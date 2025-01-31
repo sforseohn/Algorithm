dea787302d6966d2a286e65e86b914991e548133
sforseohn <100199530+sforseohn@users.noreply.github.com>
2025-02-01 02:22:30 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> nums(n + 1);
+    
+    for (int i = 1; i <= n; i++) {
+        nums[i] = i;
+    }
+    
+    while(m--) {
+        int i, j;
+        cin >> i >> j;
+        for (int k = 0; k < (j - i) / 2.0; k++) {
+            swap(nums[i + k], nums[j - k]);
+        }
+    }
+    
+    for (int q = 1; q <= n; q++) {
+        cout << nums[q] << " ";
+    }
+    
+    return 0;
+}
