3434dd358fc21946d02d1bb397331d419f506713
sforseohn <sforseohn@ewha.ac.kr>
2025-02-19 01:05:53 +0900

    [Silver I] Title: 최대공약수, Time: 180 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+ll getGcd(ll a, ll b) {
+    while (b > 0) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll a, b;
+    cin >> a >> b;
+
+    ll ans = getGcd(a, b);
+    
+    for (ll i = 0; i < ans; i++) {
+        cout << 1;
+    }
+    cout << '\n';
+   
+    return 0;
+}
