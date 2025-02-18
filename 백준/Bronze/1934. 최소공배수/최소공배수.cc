1cadbeb2239666bc0a22918df87d610169942d21
sforseohn <sforseohn@ewha.ac.kr>
2025-02-19 00:34:32 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -16,6 +16,9 @@ int getLcm(int a, int b) { // 최소공배수 = 두 수의 곱 / 최대공약수
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n, a, b;
     cin >> n;
 
