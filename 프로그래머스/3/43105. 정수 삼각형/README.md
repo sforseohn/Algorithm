f970cf54ca5c44c3227f25ecbc87576441cfe159
sforseohn <sforseohn@ewha.ac.kr>
2025-03-21 21:33:46 +0900

    [level 3] Title: 정수 삼각형, Time: 50.80 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105#) 
 
 ### 성능 요약
 
-메모리: 13.9 MB, 시간: 50.30 ms
+메모리: 14.1 MB, 시간: 50.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 21:30:42
+2025년 03월 21일 21:33:39
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,13 +1,5 @@
 def solution(triangle):
-    answer = 0
-    
-    if (len(triangle) == 1):
-        return triangle[0][0]
-    
-    triangle[1][0] += triangle[0][0]
-    triangle[1][1] += triangle[0][0]
-    
-    for i in range(2, len(triangle)):
+    for i in range(1, len(triangle)):
         for j in range(len(triangle[i])):
             # 끝 값 처리
             if j == 0:
@@ -17,4 +9,4 @@ def solution(triangle):
             else:
                 triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
     
-    return max(triangle[len(triangle) - 1])
+    return max(triangle[-1])
