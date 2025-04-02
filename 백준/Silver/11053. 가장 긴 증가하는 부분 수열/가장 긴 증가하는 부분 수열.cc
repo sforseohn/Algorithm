d732111ffc7bf202c1ebe1cea4b62520f3f8f232
sforseohn <sforseohn@ewha.ac.kr>
2025-04-03 00:47:29 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -14,15 +14,13 @@ int main()
   for (int i = 0; i < n; i++) {
     cin >> nums[i];
   }
-  
-  // 최장 증가 부분 수열 문제 (LIS)
-  // 각 요소를 순회하며 앞 요소 중 자신보다 작은 수가 있다면 dp 업데이트
+    
   for (int i = 1; i < n; i++) {
-    for (int j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        dp[i] = max(dp[i], dp[j] + 1);
+      for (int j = 0; j < i; j++) {
+          if (nums[j] < nums[i]) {
+              dp[i] = max(dp[i], dp[j] + 1);
+          }
       }
-    }
   }
 
   cout << *max_element(dp.begin(), dp.end());
