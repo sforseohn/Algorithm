f3bc332bfadfaa24680f7497b6c7f6500a3458b1
sforseohn <sforseohn@ewha.ac.kr>
2025-04-04 14:35:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,37 +3,66 @@
 
 using namespace std;
 
-typedef pair<int, int> ii;
+// 1차원 DP 배열 사용
+int knapsack1(int n, int k, vector<int>& w, vector<int>& v) {
+	// dp[i]: 배낭의 무게가 i일 때, 물건들을 넣어 얻을 수 있는 가치합의 최대값
+	vector<int> dp(k + 1, 0);
 
-int getLargestValue(int n, int k, vector<ii> &bag) {
-    int ans = 0;
-    // dp[i]: 무게 i일 때의 최대 가치
-    vector<int> dp(k + 1); // 0 ~ k
-    for (int i = 0; i < n; i++) {
-        vector<int> new_dp = dp;
-        // 가방에 넣을 수 있으면 -> 이전 거에 누적해서 저장
-        for (int j = 0; j < k; j++) {
-            if (j + bag[i].first <= k) {
-                new_dp[j + bag[i].first] = max(dp[j + bag[i].first], dp[j] + bag[i].second);
-                ans = max(ans, new_dp[j + bag[i].first]);
-            }
-        }
-        dp = new_dp;
-    }
-    return ans;
+	// i: 현재 넣으려는 물건
+	// j: 배낭의 무게
+	for (int i = 0; i < n; i++) {
+		for (int j = k; j >= w[i]; j--) {
+			// 물건을 넣는 경우 vs 넣지 않는 경우
+			int packing = dp[j - w[i]] + v[i];
+			int not_packing = dp[j];
+			dp[j] = max(packing, not_packing);
+		}
+	}
+	return dp[k];
+}
+
+// 2차원 DP 배열 사용
+int knapsack2(int n, int k, vector<int>& w, vector<int>& v) {
+	// dp[i][j]: 배낭이 버틸 수 있는 무게가 j일 때, 1~i번 물건들을 넣어 얻을 수 있는 가치합의 최대값
+	vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+
+	// i: 현재 넣으려는 물건
+	// j: 배낭이 버틸 수 있는 무게
+	for (int i = 0; i < n; i++) {
+		// 배낭이 버틸 수 있는 무게 < 물건의 무게
+		for (int j = 0; j < w[i]; j++) {
+			// 현재 물건을 넣지 못함
+			dp[i][j] = dp[i - 1][j];
+		}
+		 
+		// 배낭이 버틸 수 있는 무게 >= 물건의 무게
+		for (int j = w[i]; j <= k; j++) {
+			// 물건을 넣는 경우 vs 넣지 않는 경우
+			int packing = dp[i - 1][j - w[i]] + v[i];
+			int not_packing = dp[i - 1][j];
+			dp[i][j] = max(packing, not_packing);
+		}
+	}
+	return dp[n][k];
 }
 
 int main() {
-    int n, k;
-    cin >> n >> k;
-    vector<ii> bag(n);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); 
+	cout.tie(NULL);
 
-    for (int i = 0; i < n; i++) {
-        // 무게, 가치 입력
-        cin >> bag[i].first >> bag[i].second;
-    }
+	// 입력
+	int n, k;
+	cin >> n >> k;
 
-    cout << getLargestValue(n, k, bag);
+	vector<int> w(n);
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> w[i] >> v[i];
+	}
 
-    return 0;
+	// 연산 & 출력
+	cout << knapsack1(n, k, w, v);
+
+	return 0;
 }
