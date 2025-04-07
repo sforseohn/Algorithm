2b023408a5cb14e8ec9bb97fdbb6a3aa349c24ed
sforseohn <sforseohn@ewha.ac.kr>
2025-04-07 13:12:37 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,13 @@
-# 5 * x + 3 * y
 n = int(input())
 
 divisor = n // 5
-cnt = 0
-for i in range(divisor, -1, -1):
-    cnt = i
-    left = n - i * 5
+
+# 5 * x + 3 * y
+for x in range(divisor, -1, -1):
+    left = n - x * 5
     if left % 3 == 0:
-        cnt += left // 3
-        print(cnt)
+        y = left // 3
+        print(x + y)
         exit(0)
 
 print(-1)
