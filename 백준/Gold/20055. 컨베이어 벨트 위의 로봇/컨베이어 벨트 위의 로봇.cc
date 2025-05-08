c8d409a70cc9b7ff0f83f3831b94ba5ce3ba5c99
Seohyun Kim <sforseohn@ewha.ac.kr>
2025-05-08 18:25:41 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 108 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.cc
@@ -1,84 +1,91 @@
-#include <deque>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-struct info { // 내구도와 로봇 존재 여부
-    int power;
-    bool is_on;
-};
+int runConveyorBelt(int n, int k, vector<int> &belt)
+{
+    // 로봇 위치 bool로 저장
+    vector<bool> robot(n, false);
+    int step = 0;
 
-/* 벨트를 한 칸 회전*/
-void rotateBelt(deque<info> &belt, int n) {
-    belt.push_front(belt.back()); // belt의 마지막 칸을 앞에 push하고
-    belt.pop_back();              // 마지막 칸을 belt에서 pop
-    belt[n - 1].is_on = false; // 로봇이 내리는 위치
-}
+    while (true)
+    {
+        step++;
 
-/* 로봇을 움직일 수 있으면 한 칸 이동*/
-void moveRobot(deque<info> &belt, int n) {
-    for (int i = n - 2; i >= 0; i--) { // 배열 접근 가능 범위가 0 ~ n-1 인데 다음 칸과 비교해야 하니까 0 ~ n-2
-        if (belt[i].is_on && (!belt[i + 1].is_on) && (belt[i + 1].power >= 1)) {
-            // 지금 칸에 로봇이 존재하고, 다음 칸에 로봇이 없으며, 다음 칸에 내구도가 남아있을 때
-            belt[i].is_on = false;    // 지금 칸에 로봇 없음
-            belt[i + 1].is_on = true; // 다음 칸에 로봇 있음
-            belt[i + 1].power--;      // 다음 칸 내구도 사용
+        // 1. 벨트가 각 칸 위에 있는 로봇과 함께 한 칸 회전한다.
+        int last = belt[2 * n - 1];
+        for (int i = 2 * n - 1; i > 0; i--)
+        {
+            // q벨트 회전
+            belt[i] = belt[i - 1];
         }
-        belt[n - 1].is_on = false; // 로봇이 내리는 위치
-    }
-}
+        belt[0] = last;
 
-/* 올리는 칸에 로봇을 올릴 수 있으면 올림 */
-void putRobot(deque<info> &belt) {
-    if (!belt[0].is_on && belt[0].power >= 1) {
-        // 올리는 칸에 로봇이 존재하지 않고, 내구도가 남아있으면
-        belt[0].is_on = true; // 로봇 올리기
-        belt[0].power--;      // 내구도 사용
-    }
-}
+        for (int i = n - 1; i > 0; i--)
+        {
+            // 로봇 회전
+            robot[i] = robot[i - 1];
+        }
+        robot[0] = false;
+        // n-1에 있는 로봇은 벨트가 회전하면서 떨어짐!!!!
+        robot[n - 1] = false;
 
-/* 벨트의 내구도 체크 */
-bool checkFinish(deque<info> &belt, int n, int k) {
-    int count = 0; // 내구도 0인 칸의 개수
-    for (int i = 0; i < 2 * n; i++) {
-        if (belt[i].power == 0) { // 벨트의 능력치가 0이면
-            count++;
+        // 2. 가장 먼저 벨트에 올라간 로봇부터, 벨트가 회전하는 방향으로 한 칸 이동할 수 있다면 이동한다.
+        // 만약 이동할 수 없다면 가만히 있는다.
+        for (int i = n - 2; i >= 0; i--)
+        {
+            // 로봇이 이동하기 위해서는 이동하려는 칸에 로봇이 없으며 그 칸의 내구도가 1 이상 남아있어야 한다.
+            if (robot[i] && !robot[i + 1] && belt[i + 1] > 0)
+            {
+                robot[i + 1] = true;
+                robot[i] = false;
+                belt[i + 1]--;
+            }
+        }
+
+        // 3. 올리는 위치에 있는 칸의 내구도가 0이 아니면 올리는 위치에 로봇을 올린다.
+        if (!robot[0] && belt[0] > 0)
+        {
+            robot[0] = true;
+            belt[0]--;
+        }
+
+        // 4. 내구도가 0인 칸의 개수가 k개 이상이라면 과정을 종료한다.
+        // 그렇지 않다면 1번으로 돌아간다.
+        int zero = 0;
+        for (int i = 0; i < 2 * n; i++)
+        {
+            if (belt[i] == 0)
+            {
+                zero++;
+            }
+        }
+
+        if (zero >= k)
+        {
+            break;
         }
     }
 
-    if (count >= k) { // count가 k 이상이면 종료 조건 만족
-        return true;  // true 반환
-    }
-    return false;
+    return step;
 }
 
-int solution(deque<info> &belt, int n, int k) { // 벨트, 벨트 길이, 최대 내구도 0 개수
-    int step = 1; // 단계는 1부터 시작
-    while (true) {
-        // 회전
-        rotateBelt(belt, n); // 벨트 회전
-        // 이동
-        moveRobot(belt, n);  // 로봇 이동
-        // 로봇 올리기
-        putRobot(belt);      // 벨트에 로봇 배치
-
-        // 내구도 체크하기
-        if (checkFinish(belt, n, k)) { // 내구도 개수가 종료 조건을 만족하면
-            return step; // 몇 번째 단계가 진행 중일때 종료되었는지 반환
-        }
-        step++;              // 단계 증가
-    }
-}
-
-int main() {
+int main()
+{
     // 입력
-    int n, k; // 컨베이어 벨트 길이, 내구도 0인 칸의 최대 개수
+    int n, k;
     cin >> n >> k;
-    deque<info> belt(2 * n); // 컨베이어 벨트의 내구도와 로봇 존재 여부 저장
-    for (int i = 0; i < 2 * n; i++) { // 벨트의 칸마다
-        cin >> belt[i].power;         // 내구도 입력
-        belt[i].is_on = false;        // 로봇이 존재하는지 여부
+
+    vector<int> belt(2 * n);
+
+    // 벨트 내구도
+    for (int i = 0; i < 2 * n; i++)
+    {
+        cin >> belt[i];
     }
     // 연산 & 출력
-    cout << solution(belt, n, k);
+    cout << runConveyorBelt(n, k, belt);
+
+    return 0;
 }
